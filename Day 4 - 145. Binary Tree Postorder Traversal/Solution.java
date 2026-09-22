1ba9e968875648066cde707655f8a95b90563575
m-sanjai-183 <msanjai062@gmail.com>
2026-09-22 11:23:02 +0530

    Create Solution.java

---
+++ Day 4 - 145. Binary Tree Postorder Traversal/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+
+        postorder(root, list);
+
+        return list;
+    }
+
+    void postorder(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
+
+        postorder(root.left, list);
+        postorder(root.right, list);
+        list.add(root.val);
+    }
+}
